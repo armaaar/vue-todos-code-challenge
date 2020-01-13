e0f55cdf4a2d0bfe2fd01fd81b1f6c7d400aaa39
Ahmed Rafik Radwan <ahmedrafik_maaar@outlook.com>
2020-01-13 17:01:45 +0200

    Feat: Add Todo item when enter is pressed on <TodoForm>

--- src/components/todo-form.vue
+++ src/components/todo-form.vue
@@ -3,11 +3,28 @@
     label="Todo"
     placeholder="What do you have to do?"
     solo
+    v-model="newTodo"
+    @keydown.enter="addTodoItem"
   />
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+import Todo from '@/models/Todo';
+
+@Component
+export default class TodoForm extends Vue {
+  newTodo: String = ""
+
+  addTodoItem ():void {
+    if (this.newTodo !== "") {
+      Todo.insert({
+        data: { title: this.newTodo }
+      })
+      this.newTodo = ""
+    }
+  }
 }
 </script>
 
